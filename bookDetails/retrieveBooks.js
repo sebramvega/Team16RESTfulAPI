36379295e980b8f9d0da10ea25c49eb902ed171b
sebramvega <118126021+sebramvega@users.noreply.github.com>
2023-07-25 14:07:38 -0400

    Update retrieveBooks.js

--- bookDetails/retrieveBooks.js
+++ bookDetails/retrieveBooks.js
@@ -1,11 +1,21 @@
+/*
+Must be able to retrieve a list of books associated with an author
+o Logic: Given an Author’s Id, return the list of books for that author.
+o HTTP Request Type: GET
+o Parameters Sent: Author Id
+o Response Data: JSON list of Book Objects
+*/
+
 const express = require('express');
 const router = express.Router();
 const connection = require('./database');
 
 router.get('/', (req, res) => {
+    //Grabs the authors name from the parameters
     const authorName = req.query.authorName;
     const query = "SELECT * FROM bookTable WHERE author = ?";
 
+    //Executes SQL query to retrieve books using AuthorID while handling errors.
     connection.query(query, [authorName], (error, results) => {
         if (error) {
             console.error('Error retrieeving books for the author: ', error);
