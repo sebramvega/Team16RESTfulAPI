7aa55b290443dbc1ea5c9fe0d59ad38de1b001f8
sebramvega <118126021+sebramvega@users.noreply.github.com>
2023-07-25 14:05:09 -0400

    Update addAuthor.js

--- bookDetails/addAuthor.js
+++ bookDetails/addAuthor.js
@@ -1,8 +1,17 @@
+/*
+An administrator must be able to create an author with first name, last name, biography and publisher
+o Logic: Given an Author’s Info, add it to the system.
+o HTTP Request Type: POST
+o Parameters Sent: Author
+o Response Data: None
+*/
+
 const express = require('express');
 const router = express.Router();
 const connection = require('./database');
 
 router.post('/', (req, res) => {
+    //Extracts author from body
     const { first_name, last_name, biography, publisher } = req.body;
 
     const author = {
@@ -12,6 +21,7 @@ router.post('/', (req, res) => {
         publisher
     };
 
+    //Makes a SQL query to insert values into authorTable
     const query =  "INSERT INTO authorTable (`first_name`, `last_name`, `biography`, `publisher`) VALUES (?, ?, ?, ?)";
     const values = [
         author.first_name,
@@ -20,6 +30,7 @@ router.post('/', (req, res) => {
         author.publisher
     ];
 
+    //executes the above query with logic for error handling.\
     connection.query(query, values, (error, results) => {
         if (error) {
             console.error('Error adding author: ', error);
